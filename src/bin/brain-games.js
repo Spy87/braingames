e336a922a4cedea1536d772a6da61e0469068f36
Spy87 <k.nikitin1987@gmail.com>
2019-03-27 18:36:10 +0300

    test

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,10 @@
 #! /usr/bin/node
+
+import readlineSync from 'readline-sync';
+
 console.log("Welcome to the Brain Games!");
+
+console.log('');
+
+const user = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${user}!`);
