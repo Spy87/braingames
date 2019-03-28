21335874eb23073c27a43d0f5a77475a7f9a8f5c
Spy87 <k.nikitin1987@gmail.com>
2019-03-28 07:42:50 +0300

    test eslint

--- Makefile
+++ Makefile
@@ -3,4 +3,4 @@ install:
 start:
 	npm run babel-node -- src/bin/brain-games.js
 lint:
-	node_modules/.bin/eslint ./src/bin/*.js		
+	npm run eslint ./src/bin/*.js		
--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -2,7 +2,7 @@
 
 import readlineSync from 'readline-sync';
 
-console.log("Welcome to the Brain Games!");
+console.log('Welcome to the Brain Games!');
 
 console.log('');
 
